a4d4296fb8981a1c9764cbcf050fd0218ebc7ae6
Prakhar Dewan <prakhardewan765@gmail.com>
2022-07-16 02:18:01 +0530

    added implementation of Multiplu.java

--- Diff.java
+++ Diff.java
@@ -2,10 +2,7 @@ import java.util.Scanner;
 
 public class Diff {
     public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
-        int a = sc.nextInt();
-        int b = sc.nextInt();
-        System.out.println(a - b);
+       
     }
     
 } 
